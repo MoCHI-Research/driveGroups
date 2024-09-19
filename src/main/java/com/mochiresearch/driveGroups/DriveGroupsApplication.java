c1f4eab0190f4067052f471354b6c42261ecb8bc
Joseph Simeone <jsimeone@hamilton.edu>
2024-09-19 12:13:05 -0400

    initial commit

---
+++ src/main/java/com/mochiresearch/driveGroups/DriveGroupsApplication.java
@@ -0,0 +1,13 @@
+package com.mochiresearch.driveGroups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveGroupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveGroupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mochiresearch/driveGroups/DriveGroupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mochiresearch.driveGroups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveGroupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
